985045905b5ea301d03ae6cf2bddc863aec01890
James Lownie <jlownie@hotmail.com>
2024-10-18 13:13:30 +1100

    Added some helper functions for unit testing

--- tests/cleantext/testing_framework.py
+++ tests/cleantext/testing_framework.py
@@ -32,3 +32,26 @@ def runCleanText(source, output_reference, cleantextArgs=None):
     subprocess.run(execArgs)
 
     # Check the output
+
+# Returns a dictionary containing the default set of arguments usually received from argparse
+class defaultArgs:
+    file=None
+    urls="True"
+    markup="True"
+    output=cleanedFilePath
+
+    def __init__(self):
+        pass
+
+# Returns a dictionary to be passed into main() containing the specified set of arguments
+def getMainArgs(file, output=None, urls=None, markup=None):
+    args=defaultArgs()
+    args.file = testInputsDir + file
+    if output != None:
+        args.output=output
+    if urls != None:
+        args.urls=urls
+    if markup != None:
+        args.markup=markup
+
+    return args
